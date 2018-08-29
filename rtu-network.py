55b69c49408875795a8812e86ed7ab28492a7ec2
eugeneoca <eugenebasiliscooca@gmail.com>
2018-08-29 13:35:33 +0800

    Update

--- rtu-network.py
+++ rtu-network.py
@@ -262,6 +262,7 @@ class Client:
                         mac = hex(uuid.getnode())[2:-1]
                         mac = ':'.join(a+b for a,b in zip(mac[::2], mac[1::2]))
                         self.sock.send(sys.argv[1]+","+mac+","+ str(state_num) +"," +dt)
+                        print(sys.argv[1]+","+mac+","+ str(state_num) +"," +dt)
                         #time.sleep(2)
                     except:
                         if status == 0:
